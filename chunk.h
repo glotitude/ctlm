6d3b69582497a1f0c8ed8da987113bb2bde3e7c7
glotitude <ltoshchev@gmail.com>
2021-12-07 20:59:53 +0300

    - init

---
+++ chunk.h
@@ -0,0 +1,20 @@
+#ifndef CTLM_CHUNK_H
+#define CTLM_CHUNK_H
+
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+
+#endif //CTLM_CHUNK_H
---
+++ common.h
@@ -0,0 +1,8 @@
+#ifndef CTLM_COMMON_H
+#define CTLM_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //CTLM_COMMON_H
